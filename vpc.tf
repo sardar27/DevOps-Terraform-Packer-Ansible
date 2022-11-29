3b17aaaa9bf4a0ed879a0a10afd42c3167e72313
Mahammed Sardar <sardarmahammed5@gmail.com>
2022-11-29 11:51:02 +0530

    Completed Functions part-1

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "default" {
-  cidr_block = var.cidr_block
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
   tags = {
     Name = "${var.vpc_name}-${var.env}"
     Env  = var.env
